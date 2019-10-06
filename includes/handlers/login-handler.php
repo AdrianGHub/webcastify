9e80357883f06257834913b17103683535f880b5
Adrian <adrian_zinko@op.pl>
2019-10-06 23:07:10 +0200

    feat: handle login form

--- includes/handlers/login-handler.php
+++ includes/handlers/login-handler.php
@@ -2,6 +2,18 @@
 
 if(isset($_POST['loginButton'])) {
 	// Login button was pressed
+	$username = $_POST['loginUsername'];
+	$password = $_POST['loginPassword'];
+
+	
+	// check if login was successful
+	$result = $account->login($username, $password);
+
+	// if successful
+	if($result) {
+		// Redirect to index page
+		header("Location: index.php");
+	}
 }
 
 ?>
--- register.php
+++ register.php
@@ -28,12 +28,13 @@ function getInputValue($name) {
 	<form id="loginForm" action="register.php" method="POST">
     	<h2>Login to your account</h2>
     		<p>
+			<?php echo $account->getError(Constants::$loginFailed);  ?>
 				<label for="loginUsername">Username</label>
     			<input id="loginUsername" name="loginUsername" type="text" placeholder="e.g. bartSimpson" required>  
     		</p>
     		<p>
-				<label for="passwordUsername">Password</label>
-    			<input id="passwordUsername" name="passwordUsername" type="password" placeholder="Your password" required>
+				<label for="loginPassword">Password</label>
+    			<input id="loginPassword" name="loginPassword" type="password" placeholder="Your password" required>
 			</p>
 			
 			<button type="submit" name="loginButton">LOG IN</button>
