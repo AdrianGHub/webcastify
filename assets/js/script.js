cd8fed9e0ead6ab2037802cca02fbb902e41fb90
Adrian <adrian_zinko@op.pl>
2019-10-29 09:12:53 +0100

    feat: track function setup

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,11 +1,37 @@
+var currentPlaylist = [];
+var audioElement;     
+
+
+// class Audio {
+
+//     constructor(currentPlaying, audio) {
+//         this.currentlyPlaying = currentPlaying;
+//         this.audio = audio;
+//         audio = document.createElement('audio');
+//     }
+//     setTrack(src) {
+//         return this.audio.src = src;
+//     }
+
+// }
+
+
 function Audio() {
 
-    this.currentlyPlaying;
-    this.audio = document.createElement('audio');
+        this.currentlyPlaying;
+        this.audio = document.createElement('audio');
+    
+    
+        this.setTrack = function(src) {
+            this.audio.src = src;
+    }
 
-    this.setTrack = function(src) {
-        this.audio.src = src;
+    this.play = function() {
+        this.audio.play();
+    }
 
+    this.pause = function() {
+        this.audio.pause();
     }
 
 }
