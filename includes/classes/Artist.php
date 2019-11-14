0268b661bfacc69b701dde28a416e4982d588bf2
Adrian <adrian_zinko@op.pl>
2019-11-14 10:35:30 +0100

    fix: correct db query

--- includes/classes/Artist.php
+++ includes/classes/Artist.php
@@ -12,7 +12,7 @@
     
     public function getName() {
 
-    $artistQuery = mysqli_query($this->con, "SELECT name FROM artist WHERE id='$this->id'");
+    $artistQuery = mysqli_query($this->con, "SELECT name FROM artists WHERE id='$this->id'");
     $artist = mysqli_fetch_array($artistQuery);
     return $artist['name'];
                                     
