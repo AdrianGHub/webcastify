4f7c174d708d12618711652d9eeed463fc414d0f
Adrian <adrian_zinko@op.pl>
2019-12-21 17:54:07 +0100

    feat: update user details form

---
+++ updateDetails.php
@@ -0,0 +1,27 @@
+<?php
+include("includes/includedFiles.php");
+?>
+
+<div class="userDetails">
+
+    <div class="container borderBottom">
+
+    <h2>EMAIL</h2>
+    <input type="text" class="email" name="email" placeholder="Adres email..." value="<?php echo $userLoggedIn->getEmail(); ?>">
+    <span class="message"></span>
+    <button class="settingsButton" onclick="">SAVE</button>
+
+    </div>
+
+    <div class="container">
+
+    <h2>PASSWORD</h2>
+    <input type="password" class="oldPassword" name="oldPassword" placeholder="Aktualne hasło...">
+    <input type="password" class="newPassword1" name="newPassword1" placeholder="Nowe hasło...">
+    <input type="password" class="newPassword2" name="newPassword2" placeholder="Potwierdź hasło...">
+    <span class="message"></span>
+    <button class="settingsButton" onclick="">SAVE</button>
+    
+    </div>
+
+</div>
